b94f2214c998218caee8d5b5dbd23097ba86a550
Hayeon001 <khy8723@gmail.com>
2023-02-13 09:36:11 +0900

    ver.1

---
+++ 0209/switch.js
@@ -0,0 +1,20 @@
+let food = "carrot";
+switch (food) {
+    case "melon":
+        console.log("fruit");
+        break;
+    case "apple":
+        console.log("fruit");
+        break;
+    case "banana":
+        console.log("fruit");
+        break;
+    case "carrot":
+        console.log("vegetable");
+        break;
+    default:
+        console.log("It's not fruits and vegetables.");
+        break;
+}
+// switch 첫번째 casa 맞으면 break. 아니면 지나가. 다음 case.
+// 부합하지 않을 경우 입력할 문구 default
